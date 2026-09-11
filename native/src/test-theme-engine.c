57e8cbef75181c9032b37497b9a149ff8686f4b0
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-09-11 15:08:22 -0500

    test(native): cover Theme Engine persistence

--- native/src/test-theme-engine.c
+++ native/src/test-theme-engine.c
@@ -1,10 +1,23 @@
 #include <glib.h>
+#include <glib/gstdio.h>
 
 #include "theme-engine.h"
+
+static char *test_directory;
+static char *test_settings_path;
+
+static void
+reset_persistence(void)
+{
+    if (test_settings_path != NULL)
+        g_unlink(test_settings_path);
+}
 
 static void
 test_default_theme(void)
 {
+    reset_persistence();
+
     GoreeTerminalThemeEngine engine;
     goree_terminal_theme_engine_init(&engine);
 
@@ -15,6 +28,8 @@ test_default_theme(void)
 static void
 test_theme_selection(void)
 {
+    reset_persistence();
+
     GoreeTerminalThemeEngine engine;
     goree_terminal_theme_engine_init(&engine);
 
@@ -55,13 +70,82 @@ test_glaze_personalization_surface(void)
     g_assert_nonnull(deep->background);
 }
 
+static void
+test_theme_persistence(void)
+{
+    reset_persistence();
+
+    GoreeTerminalThemeEngine first;
+    goree_terminal_theme_engine_init(&first);
+    g_assert_true(goree_terminal_theme_engine_set(&first, "deep-dark"));
+    g_assert_true(g_file_test(test_settings_path, G_FILE_TEST_IS_REGULAR));
+
+    GoreeTerminalThemeEngine second;
+    goree_terminal_theme_engine_init(&second);
+    g_assert_cmpint(
+        goree_terminal_theme_engine_get(&second),
+        ==,
+        GOREE_TERMINAL_THEME_DEEP_DARK);
+
+    gchar *contents = NULL;
+    gsize length = 0;
+    g_assert_true(g_file_get_contents(test_settings_path, &contents, &length, NULL));
+    g_assert_nonnull(g_strstr_len(contents, (gssize) length, "mode=deep-dark"));
+    g_assert_null(g_strstr_len(contents, (gssize) length, "command"));
+    g_assert_null(g_strstr_len(contents, (gssize) length, "history"));
+    g_free(contents);
+}
+
+static void
+test_invalid_persistence_fails_safe(void)
+{
+    reset_persistence();
+    g_assert_true(g_file_set_contents(
+        test_settings_path,
+        "[Theme]\nmode=not-a-theme\n",
+        -1,
+        NULL));
+
+    g_test_expect_message(
+        G_LOG_DOMAIN,
+        G_LOG_LEVEL_WARNING,
+        "*theme preference is invalid*Follow System*");
+
+    GoreeTerminalThemeEngine engine;
+    goree_terminal_theme_engine_init(&engine);
+
+    g_test_assert_expected_messages();
+    g_assert_cmpint(
+        goree_terminal_theme_engine_get(&engine),
+        ==,
+        GOREE_TERMINAL_THEME_FOLLOW_SYSTEM);
+}
+
 int
 main(int argc, char **argv)
 {
+    GError *error = NULL;
+    test_directory = g_dir_make_tmp("goreecloud-terminal-theme-XXXXXX", &error);
+    g_assert_no_error(error);
+    g_assert_nonnull(test_directory);
+
+    test_settings_path = g_build_filename(test_directory, "theme.ini", NULL);
+    g_setenv("GOREE_TERMINAL_THEME_SETTINGS_PATH", test_settings_path, TRUE);
+
     g_test_init(&argc, &argv, NULL);
     g_test_add_func("/theme/default", test_default_theme);
     g_test_add_func("/theme/selection", test_theme_selection);
     g_test_add_func("/theme/system-resolution", test_system_resolution);
     g_test_add_func("/theme/glaze-personalization-surface", test_glaze_personalization_surface);
-    return g_test_run();
+    g_test_add_func("/theme/persistence", test_theme_persistence);
+    g_test_add_func("/theme/invalid-persistence-fails-safe", test_invalid_persistence_fails_safe);
+
+    int result = g_test_run();
+
+    reset_persistence();
+    g_unsetenv("GOREE_TERMINAL_THEME_SETTINGS_PATH");
+    g_free(test_settings_path);
+    g_rmdir(test_directory);
+    g_free(test_directory);
+    return result;
 }
